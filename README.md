0e1a71cfcad925765bbb469b9947fed9fd5ddcf1
halfMonkey <mhyadai@gmail.com>
2020-12-16 14:39:12 -0500

    script

--- README.md
+++ README.md
@@ -1,25 +1,16 @@
-# LeetCode-Note
+# Leet Code Note 
+
+### ./12/15/2020
+
+[15.3Sum](https://leetcode.com/problems/3sum/)
+		[Source Code](./12-15-2020/3Sum)
+[3.LongestSubstringWithoutRepeatingCharacters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
+		[Source Code](./12-15-2020/Longest-Substring-Without-Repeating-Characters)
+[763.PartitionLabels](https://leetcode.com/problems/partition-labels/)
+		[Source Code](./12-15-2020/Partition-Labels)
+[42.TrappingRainWater](https://leetcode.com/problems/trapping-rain-water/)
+		[Source Code](./12-15-2020/Trapping-Rain-Water)
+[1.TwoSum](https://leetcode.com/problems/two-sum/)
+		[Source Code](./12-15-2020/twosum)
 
 
-
-### 12/15/2020
-
-[1. Two Sum](https://leetcode.com/problems/two-sum/)
-
-​		[Source code](./12-15-2020/twosum)
-
-[42. Trapping Rain Water](https://leetcode.com/problems/trapping-rain-water/)
-
-​		[Source code](./12-15-2020/Trapping-Rain-Water)
-
-[3. Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
-
-​		[Source code](./12-15-2020/Longest-Substring-Without-Repeating-Characters)
-
-[15. 3Sum](https://leetcode.com/problems/3sum/)
-
-​		[Source code](./12-15-2020/3Sum)
-
-[763. Partition Labels](https://leetcode.com/problems/partition-labels/)
-
-​		[Source code](./12-15-2020/Partition-Labels)
---
+++ TaskListGenerate.py
@@ -0,0 +1,51 @@
+import os
+
+def markDown( folderName:str )->str:
+    res = ""
+    date = folderName.replace('-', '/')
+    res = "### " + date + "\n" + '\n'
+    files = openFolder(folderName)
+    for name in files:
+        res += '[' + name + ']' + '(' + files[name][0] + ')' +'\n'
+        res +=  '\t\t' + '[Source Code]' + '(' + files[name][1] + ')' + '\n' 
+
+    res += '\n'
+    return res
+
+
+def openFolder(folderName):
+    res = {}
+    for file in os.listdir(folderName):
+        path = folderName + '/' + file
+        #res.append(folderName + '/' + file)
+        f = open(path)
+        name = f.readline()
+        link = f.readline()
+        if name[0] != '#':
+            name = file
+        else:
+            name = name.replace('\n', '')[1:]
+            tk = name.split(' ')
+            name = "".join([tk[i] for i in range(0, len(tk)) if tk[i] != '' ])
+        
+        if link[0] != '#':
+            link = path
+        else:
+            link = link.replace('\n', '')[1:]
+            tk = link.split(' ')
+            link = "".join([tk[i] for i in range(0, len(tk)) if tk[i] != '' ])
+        
+        res[name] = [link, path]
+
+    return res
+
+ct = "# Leet Code Note \n\n"
+
+for x in os.listdir('.'):
+    if os.path.isdir('./' + x) and x != '.git':
+        ct += markDown('./' + x)
+        ct += "\n"
+
+f = open('README.md', 'w')
+f.write(ct)
+f.close()
